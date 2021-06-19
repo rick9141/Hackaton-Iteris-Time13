0b47cbb5c53f96f38a51479348b5d7fff5cf868a
Elgtha Priscilla <epbq12@gmail.com>
2021-06-19 15:11:14 -0300

    fazendo correções

--- HackatonIteris/src/app/app-routing.module.ts
+++ HackatonIteris/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuTemplateComponent } from './components/template/menu-template/menu-template.component';
-<<<<<<< HEAD
-import { HomePageComponent } from './components/home-page/home-page.component';
 import { ContatosEmergenciaComponent } from './components/template/contatos-emergencia/contatos-emergencia.component';
-=======
 import { HomePageComponent } from './components/template/home-page/home-page.component';
 import { SobrePageComponent } from './components/template/sobre-page/sobre-page.component';
->>>>>>> d54ebe2f6492c0569d669e3563c4bb56a9afefff
 
 
 const routes: Routes = [{
@@ -19,15 +15,13 @@ const routes: Routes = [{
       component: HomePageComponent
     },
     {
-<<<<<<< HEAD
       path: 'Contato',
       component: ContatosEmergenciaComponent
     },
-=======
+    {
       path: 'sobre',
       component: SobrePageComponent
     }
->>>>>>> d54ebe2f6492c0569d669e3563c4bb56a9afefff
   ]
 }];
 
